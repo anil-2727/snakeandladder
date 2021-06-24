533cdd4522bb2d860abe3fa381a6b5a11bbcd8c1
anil-2727 <bali39959@gmail.com>
2021-06-24 13:20:00 +0530

    starting position

---
+++ Snakeandladder.java
@@ -0,0 +1,11 @@
+package ladder;
+
+public class Snakeandladder {
+
+	public static void main(String[] args){
+		int position = 0;
+		System.out.println("Welcome to Snake-Ladder Program Problem");
+		System.out.println("You are at Starting Position::" + position);
+	}
+
+}
