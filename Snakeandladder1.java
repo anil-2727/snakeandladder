6cd2a190906535b45030f79f64fb20fb2371df45
anil-2727 <bali39959@gmail.com>
2021-06-24 13:24:02 +0530

    die roll

---
+++ Snakeandladder1.java
@@ -0,0 +1,17 @@
+package ladder;
+
+public class Snakeandladder1 {
+
+public static void main(String[] args)
+{
+
+	int Position=0;
+	System.out.println("Welcome to Snake-Ladder Program Problem");
+	System.out.println("You are at Starting Position::"+Position);
+}
+	public static void Die()
+	{
+		int DieCheck = 1 +(int) Math.floor(Math.random()*6);
+		System.out.println("Dice Value::"+ DieCheck);
+	}
+}
